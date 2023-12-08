94b26556eeaef6c7e4dba2b4b92ab24322033895
Md. Mosharaf Hossain Apurbo <aoarish397@gmail.com>
2023-12-09 00:57:00 +0600

    Add week_1

---
+++ week_1/online_0.cpp
@@ -0,0 +1,29 @@
+// Implementing bfs algorithm
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+#define trav(a, x) for(auto& a : x)
+#define FOR(i, a, b) for (int i=(a); i<=(signed)(b); i++)
+#define ROF(i, a, b) for (int i=(a); i>=(signed)(b); i--)
+#define F0R(i, a) for (int i=0; i<(signed)(a); i++)
+#define vi vector<int>
+#define f first
+#define s second
+#define all(v) (v).begin(), (v).end()
+typedef long long ll;
+
+const ll mod = 1e9 + 7, MOD = 998244353;
+
+void solve() {
+    // Your solution goes here
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
