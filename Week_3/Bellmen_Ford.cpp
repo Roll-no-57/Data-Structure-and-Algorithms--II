79cf5bdefdd82bfcee3e33bcf6c807916bd45508
Md. Mosharaf Hossain Apurbo <aoarish397@gmail.com>
2023-12-17 01:23:23 +0600

    updated

---
+++ Week_3/Bellmen_Ford.cpp
@@ -0,0 +1,113 @@
+// implementing Dijkstra's algorithm using priority queue
+//  Time Complexity: O(ElogV)
+//  Space Complexity: O(V+E)
+//  Author: Mosharaf Hossain Apurbo
+//  Roll : 2105057
+//  Date : 09/12/2023
+/*.................................................*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	/*  Function to implement Bellman Ford
+	*   edges: vector of vectors which represents the graph
+	*   S: source vertex to start traversing graph with
+	*   V: number of vertices
+	*/
+	vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S,vector<int> &parent) {
+		vector<int> dist(V, 1e8);
+		dist[S] = 0;
+        parent[S]=S;
+
+		for (int i = 0; i < V - 1; i++) {
+			for (auto it : edges) {
+				int u = it[0];
+				int v = it[1];
+				int wt = it[2];
+				if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+					dist[v] = dist[u] + wt;
+                    parent[v]=u;
+				}
+			}
+		}
+
+
+		// Nth relaxation to check negative cycle
+		for (auto it : edges) {
+			int u = it[0];
+			int v = it[1];
+			int wt = it[2];
+			if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+				return { -1};
+			}
+		}
+
+
+		return dist;
+	}
+
+    void printRoute(vector<int> &parent,int Start,int destination){
+        vector<int> ans;
+
+        cout<<"Route from "<<Start<<" to "<<destination<<" : ";
+
+        while(Start!=destination){
+            ans.push_back(destination);
+            destination = parent[destination];
+        }
+        reverse(ans.begin(),ans.end());
+
+        cout<<Start<<"->";
+        for(auto i:ans){
+            cout<<i<<"->";
+        }
+        cout<<"Destination"<<endl;
+    }
+
+};
+
+
+int main() {
+
+	int V ,E;cin>>V>>E;
+	vector<vector<int>> edges(E, vector<int>(3));
+    vector<int> parent(V,-1);
+
+
+	for(int i=0;i<E;i++){
+        cin>>edges[i][0]>>edges[i][1]>>edges[i][2];
+    }
+
+	int S,destination;cin>>S>>destination;
+	Solution obj;
+	vector<int> dist = obj.bellman_ford(V, edges, S,parent);
+	for (auto d : dist) {
+		cout << d << " ";
+	}
+	cout << endl;
+
+    obj.printRoute(parent,S,destination);
+
+	return 0;
+}
+
+/*
+test case :
+4 4 
+1 2 10
+2 3 12
+3 4 -5
+4 1 -5
+1 4
+
+5 7 
+0 1 7
+0 2 1
+0 3 2
+1 2 3
+1 3 5
+1 4 1
+3 4 7
+0 4
+*/
--- Week_3/Dijkstra_priority_queue.cpp
+++ Week_3/Dijkstra_priority_queue.cpp
@@ -9,21 +9,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 class Solution
 {
 public:
-    // Function to find the shortest distance of all the vertices
-    // from the source vertex S.
 
-    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    vector<int> dijkstra(int V, vector<vector<int>> adj[], int S,vector<int> &parent)
     {
-        //
+
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
         vector<int> dist(V, 1e9);
 
         dist[S] = 0;
+        parent[S]=S;
 
         pq.push({0, S});
 
@@ -45,44 +43,79 @@ public:
                 {
                     dist[adjNode] = dis + edgeWeight;
                     pq.push({dist[adjNode], adjNode});
+                    parent[adjNode]= node;
                 }
+
             }
+
         }
         return dist;
     }
-};
 
+    void printRoute(vector<int> &parent,int Start,int destination){
+        vector<int> ans;
+
+        cout<<"Route from "<<Start<<" to "<<destination<<" : ";
+
+        while(Start!=destination){
+            ans.push_back(destination);
+            destination = parent[destination];
+        }
+        reverse(ans.begin(),ans.end());
+
+        cout<<Start<<"->";
+        for(auto i:ans){
+            cout<<i<<"->";
+        }
+        cout<<"Destination"<<endl;
+    }
+
+};
 
 int main()
 {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
     int tc;
     cin >> tc;
     while (tc--)
     {
-        int V, E, S;
-        cin >> V >> E >> S;
+        int V, E, S,destination;
+        cin >> V >> E >> S>>destination;
 
         vector<vector<int>> adj[V];
+        vector<int> parent(V,-1);
 
         for(int i=0;i<E;i++){
             int u,v,w;
             cin>>u>>v>>w;
 
             adj[u].push_back({v,w});
-            adj[v].push_back({u,w});
+            // adj[v].push_back({u,w});
         }
 
-        cout<<"Journey starts from "<<S<<endl; 
         Solution obj;
-        vector<int> ans = obj.dijkstra(V,adj,S);
+        vector<int> ans = obj.dijkstra(V,adj,S,parent);
 
         for(int i=0;i<V;i++){
             cout<<ans[i]<<" ";
         }
         cout<<endl;
 
+        obj.printRoute(parent,S,destination);
+
     }
 }
+/*
+1
+5 7 0 4
+0 1 7
+0 2 1
+0 3 2
+1 2 3
+1 3 5
+1 4 1
+3 4 7
+
+*/
